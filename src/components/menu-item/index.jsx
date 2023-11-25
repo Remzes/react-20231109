349bda0f7d0157c8442f9a56f8f4801c4c8793da
Remzes <victor_s1998@mail.ru>
2023-11-25 16:02:05 +0300

    added counter for menu items

--- src/components/menu-item/index.jsx
+++ src/components/menu-item/index.jsx
@@ -1,9 +1,12 @@
+import { MenuItemCounter } from "../counter"
+
 export const MenuItem = ({ name, price, ingredients }) => {
     return (
         <>
             <p>
                 <strong>{name}</strong> - {price}$
             </p>
+            <MenuItemCounter />
             <p>{ingredients}</p>
         </>
     )
