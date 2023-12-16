ca2e9ebdba3c52d28f2d060f3cbab997d0780efe
Viktor S <victor_s1998@mail.ru>
2023-12-16 20:05:02 +0300

    Updated theme provider

--- src/providers/themeProvider.jsx
+++ src/providers/themeProvider.jsx
@@ -4,20 +4,17 @@ import { THEME_CONST } from "../consts/THEME_CONST"
 
 export const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState(THEME_CONST.light)
-    
-    const updateTheme = useCallback((theme) => setTheme(theme), [])
 
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', THEME_CONST.light)
     }, [])
 
-    useLayoutEffect(() => {
-        console.log('Changed!', theme)
+    useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme)
     }, [theme])
 
     return (
-        <ThemeContext.Provider value={{ theme, updateTheme }}>
+        <ThemeContext.Provider value={{ theme, updateTheme: setTheme }}>
             { children }
         </ThemeContext.Provider>
     )
